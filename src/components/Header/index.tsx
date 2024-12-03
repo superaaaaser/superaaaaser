a97cd4edf455a5faf40a74c853e75cb7fe32c468
superaaaaser <superaaaaser@gmail.com>
2024-12-03 15:46:48 +0700

    superaaaaser

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,12 +4,12 @@ import Button from "../ui/Button";
 export default function Header() {
   return (
     <>
-      <div className="w-full lg:  px-24 flex justify-between h-16 items-center bg-white dark:bg-zinc-950">
+      <div className="w-full px-4 lg:px-24 flex justify-between h-16 items-center bg-white dark:bg-zinc-950">
         <div className="font-semibold text-xl flex items-center gap-1">
-          <span className="material-symbols-outlined">
+          <span className="material-symbols-outlined text-zinc-100 ">
             keyboard_command_key
           </span>
-          <span>SuperaseR</span>
+          <span className="text-zinc-100">SuperaseR</span>
         </div>
 
         <div>
