d1d2d0f7f85ff39d78fbccc6c85bbda538083309
superaaaaser <superaaaaser@gmail.com>
2024-12-03 12:33:35 +0700

    superaaaaser

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,3 +1,10 @@
 export default function Hero() {
-  return <div className="w-full  h-[calc(100vh-64px)]"></div>;
+  return (
+    <div className="w-full  h-[calc(100vh-64px)] flex flex-col px-24 bg-white gap-y-9 ">
+      <div className="lg:w-full h-2/4 shadow-no-blur border border-zinc-900">
+        jkjkj
+      </div>
+      <div>hjjhjh</div>
+    </div>
+  );
 }
